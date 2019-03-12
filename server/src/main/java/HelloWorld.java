1d417c8e9506f38139ff3f838826cfe34e34804f
Ruslan <r.rudnev@gmail.com>
2019-03-12 13:41:16 -0400

    spring boot runs

---
+++ server/src/main/java/HelloWorld.java
@@ -0,0 +1,26 @@
+package io.cognizant.hmh.asdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld.class, args);
+	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//			registry.addResourceHandler("/pages/**").addResourceLocations("classpath:/static/");
+//	}
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//			registry.addViewController("/").setViewName("redirect:/pages/index.html");
+//	}
+}
